23cd3c2b8d0ac03c4f6f6d8ad4bdd9bed57cdf4d
zardari5647 <145971005+zardari5647@users.noreply.github.com>
2024-03-17 21:04:04 +0000

    Add files via upload

---
+++ www/index1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>  
+      
+    <title>Index Page</title>
+    <link rel="stylesheet" href="CSS/index2.css">
+  </head>
+    
+    <h1> Mood for Food</h1>
+
+<div class="navbar">
+  <a href="menu.php">Menu</a>
+     <a href="Aboutus.php">About us</a>
+      
+     
+  <div class="dropdown">
+    <button class="dropbtn">Special Diets 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Vegan</a>
+      <a href="#">Vegetarian</a>
+      <a href="#">High-Protein</a>
+        <a href="#">Halal</a>
+        <a href="#">Low in calories</a>
+    </div>
+  </div> 
+</div>
+
+      <br>
+
+  
+      <div class="chunks-container">
+          
+        <div class="item1">
+          <h2>Welcome to MoodForFood</h2>
+            
+            
+            
+              
+            <div id="left"> <img src="Images/download.png" alt="shop sign"  width="400" height="400"> </div>
+            
+            
+                
+
+
+             
+             
+                 
+            
+            
+            
+            <a href="index.php" class="button">Login</a>
+            
+            
+            
+            
+          
+      </div> 
+          
+          
+                      <div class="item3">
+            <p2>shgcuhgyg dhegu dehuedh ihdiwuedhu wdiruhwiruh dewidgd iwdhiue beiudb edbwid edgui</p2> 
+             
+        </div>
+    </div>
+    
+    <br>
+    <br>
+ 
+      <footer>
+        <br>
+        <br>
+         <h2>Mood For Food </h2>  
+          
+          <p class="contact"> <a href="menu.php">Menu</a>  |<a href="Aboutus.php">about us</a> </p>
+          
+       <h1>Contact us</h1>
+        
+           <img src="Images/call.png" alt="shop sign"  width="25" height="25"> <br>
+          <p> +123456789</p>
+            <img src="Images/envelope.png" alt="shop sign"  width="25" height="25"> <p> <a href="mailto:MoodForFood@hotmail.com"> MoodForFood@gmail.com</a></p><br>
+          
+          <img src="./marker1.png" alt="shop sign"  width="25" height="25"> <p> Dublin1, City Centre</p>
+      </footer>
+ 
+    
+  </body>
+</html>
---
+++ www/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    
+    <?php
+ 
+?>
+    <head>
+       
+
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+        <link rel="stylesheet"
+          href="CSS/login.css">
+
+      
+           
+        </style>
+    </head>
+    <body>
+    <fieldset>
+            
+        <h1> Login</h1>
+        <br><br><br><br><br>
+    <form  method="post"  action="">
+  <label for="email">Email:</label> &nbsp;&nbsp;&nbsp;&nbsp;
+  <input type="text" id="email" name="email" value="" > <br> <br>
+        
+         
+           
+        <br><br>
+        
+ <label for="address">Password:</label>
+  <input type="password" id="address" name="address" ><br><br> &nbsp;&nbsp;&nbsp;
+       
+        <p>need an account?</p> <a href="signup.php">Signup</a>
+        
+        <br><br>
+        
+        
+    <a href="index.php" class="button">Login</a>
+      
+        
+
+
+        
+ 
+
+ 
+    </form>
+        
+     </fieldset>
+</body>
+</html>
