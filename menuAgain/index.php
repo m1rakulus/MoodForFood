7e9c23089a01a3adc903096b1d71c84a9c523f83
janouskie <b00149565@mytudublin.ie>
2024-04-28 14:59:44 +0100

    fixed(?)

--- menuAgain/index.php
+++ menuAgain/index.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     // $connect = mysqli_connect("localhost,"root","jannabanana500!!","menutest");
+    //changes made
     /**
     * Configuration for database connection
     *
@@ -10,7 +11,7 @@
         if(isset($_SESSION['cart'])){
             $session_array_id = array_column($_SESSION['cart'], "id");
 
-            if(!in_array($_GET['id'], session_array_id)){
+            if(!in_array($_GET['id'], $session_array_id)){
 
                 $session_array = array(
                     'id'  => $_GET['id'],
@@ -54,10 +55,10 @@
                             <div class="col-md-4">
                                 <form method="post" action="index.php?id=<?=$row['id'] ?>">
                                     <img src="images/<?=$row['image'] ?>" style='height: 150px; width: auto' />
-                                    <h5 class="text-center"><?= $row['name']; ?></h5>
+                                    <h5 class="text-center"><?= $row['prodName']; ?></h5>
                                     <h5 class="text-center">€<?= number_format($row['price'],2); ?></h5>
 
-                                    <input type="hidden" name="name" value="<?= $row['name'] ?>">
+                                    <input type="hidden" name="name" value="<?= $row['prodName'] ?>">
                                     <input type="hidden" name="price" value="<?= $row['price'] ?>">
                                     <input type="number" name="quantity" value="1" class="form-control">
 
@@ -108,7 +109,7 @@
                                 <td></b>Total Price</b></td>
                                 <td>".number_format($total,2)."</td>
                                 <td>
-                                    <a href=''index.php?action=clearall>
+                                    <a href='index.php?action=clearall'>
                                         <button class='btn btn-warning btn-block'>Clear all</button>
                                     </a>
                                 </td>
